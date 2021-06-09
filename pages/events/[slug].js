efd95f061fcbd5f512d1fb54494b8a57aef275dd
Lazar <donickoce@gmail.com>
2021-06-09 09:47:45 +0200

    next link and routing

--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -1,7 +1,16 @@
+import { useRouter } from "next/router";
+
 export default function EventPage() {
-    return (
-        <div>
-            <h1>My Event</h1>
-        </div>
-    )
+
+    const router = useRouter();
+
+    console.log(router);
+
+  return (
+    <div>
+      <h1>My Event</h1>
+      <h3>{router.query.slug}</h3>
+      <button onClick={() => router.push('/')}>Back to Home</button>
+    </div>
+  );
 }
