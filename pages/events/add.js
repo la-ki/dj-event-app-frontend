db441d9ea882e3169d8cce15c163a36bf5d5b0bc
Lazar <donickoce@gmail.com>
2021-06-13 14:30:22 +0200

    Event Submit, Added React Toastify

--- pages/events/add.js
+++ pages/events/add.js
@@ -1,3 +1,5 @@
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import Layout from "@/components/Layout";
 import { useState } from "react";
 import { useRouter } from "next/router";
@@ -18,9 +20,32 @@ export default function AddEventPage() {
 
   const router = useRouter();
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log(values);
+    // Validation
+    const hasEmptyFields = Object.values(values).some(
+      (element) => element === ""
+    );
+
+    if (hasEmptyFields) {
+      toast.error("Please fill in all fields");
+    }
+
+    const res = await fetch(`${API_URL}/events`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(values)
+    })
+
+    if (!res.ok) {
+        toast.error('Something Went Wrong')
+    } else {
+        const evt = await res.json()
+        router.push(`/events/${evt.slug}`)
+    }
+
   };
 
   const handleInputChange = (e) => {
@@ -32,6 +57,17 @@ export default function AddEventPage() {
     <Layout title="Add New Event">
       <Link href="/events">Go Back</Link>
       <h1>Add Event</h1>
+      <ToastContainer
+        position="top-right"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
       <form onSubmit={handleSubmit} className={styles.form}>
         <div className={styles.grid}>
           <div>
